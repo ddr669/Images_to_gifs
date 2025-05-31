3224532bdc4a2e6949fc53c0971ec45a6c1a4721
ddr669 <dudu.herique.x@gmail.com>
2025-05-31 17:49:25 -0300

    .

--- transforma_gif.py
+++ transforma_gif.py
@@ -6,49 +6,64 @@
 from PIL import Image
 import cv2
 from sys import argv
-from pygame import Surface, surfarray, SRCALPHA, font, draw, Rect
+from pygame import Surface, surfarray, SRCALPHA, font, draw, Rect, image
+from pygame.transform import scale
+from pygame.sprite import Sprite, Group
 from cmdline_verify import cmdline_verify
 from cmdline_verify import __help__, return_file_
+
+class Sprites_(Sprite):
+    def __init__(self, color=(0,0,0,0), height: int = None, width: int = None,
+                 _file: str = None,
+                 colorkey:list=[]):
+        super().__init__()
+        self.image = image.load(_file) if _file else Surface([width, height])
+        if colorkey:
+            
+            self.image.set_colorkey(self.image.get_at((100,100)))
+            print(self.image.get_at((10,10)))
+        if width:
+            draw.rect(self.image,color,Rect(0,0,width,height))
+
+        self.rect = self.image.get_rect()
+        if self.rect[-1] >= 720 or self.rect[-2] >= 1024:
+            self.image = scale(self.image, (720, 500))
+            self.rect = self.image.get_rect()
+        
 
 # xxxxxxxxxxxxxxxxx
 # make a texture to image with pygame.Surface
 # from the given args (_font, _size, _from, _to, size:tuple=(20,20) repeat: bool=False, text: str= "xxx")
-def make_image_from_fonts(_font: str = "lucidaconsole", 
+def make_image_from_fonts(file, _font: str = "lucidaconsole", 
                         _size: int = 12, _from = None,
-                        _to = None, size: tuple = (600,600), 
-                        repeat: bool = True, _text: str = "Vocês são gays!",
-                        _test_: bool = True,
-                        _color_: tuple = (0,0,0) ) :
+                        _to = None, size: tuple = None, 
+                        repeat: bool = True, _text: str = "xxx",
+                        _test_: bool = False,
+                        _color_: tuple = (0,0,0), _colorkey_: tuple = (0,0,0) ) :
+    ############# TEste ###################
+    sprite_group = Group()
+    _img = Sprites_(_file=file, colorkey=(200,0,0))
+    sprite_group.add(_img)
+    size = _img.image.get_rect()[2:]
     
-    if _test_:
-        arraysurf = Surface(size, SRCALPHA)
-        font.init()
-        font_pygame = font.SysFont(_font, _size)
-        texto = font_pygame.render(_text, True, _color_)
-        arraysurf.fill((0,0,0))
-        for a in range(0, 600, 25):
-            draw.line(arraysurf, (255,255,255), (a, 0), (a, 1200))
-            
-        re = [((0,400),(600, 200)), ((0,0),(600, 200)), ((0,200),(600, 200))]
-        co = [(255,0,0), (0,255,0), (0,0,255)]
-        for a in range(0, 3):
-            draw.rect(arraysurf, co[a], re[a], 0)
-        if repeat:
-            for y in range(0, size[1], _size):
-                for x in range(0, size[0], len(_text)*7):
-                    arraysurf.blit(texto, (x, y))
+    arraysurf = Surface((size[0],size[1]), SRCALPHA)
+    font.init()
+    font_pygame = font.SysFont(_font, _size)
+    texto = font_pygame.render(_text, True, _color_)
+    arraysurf.fill((0,0,0))    
         # FINALY!
-        
-        view = surfarray.array3d(arraysurf)
+    sprite_group.draw(arraysurf)    
+    view = surfarray.array3d(arraysurf)
 
-        view = view.transpose([1, 0 , 2])
-        img_bgr = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
-        cv2.imshow("image",img_bgr)
+    view = view.transpose([1, 0 , 2])
+    img_bgr = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
+    cv2.imshow("image",img_bgr)
 
-        cv2.waitKey(0)
-        cv2.imwrite("out.jpg", img_bgr)
+    cv2.waitKey(0)
+    cv2.imwrite(_to if _to else "out.jpg", img_bgr)
+    ############## test End ##################
     return 1
-
+    #####################################
 def read_dir_(path: str, ext: str = None):
     # to make the things izi, if you named the pics files with * img_0001.jpg * 
     # ( Optional : if you dont choose extension, will be necessary!)
@@ -79,8 +94,8 @@ def main(file_dict: dict):
     # END
 if __name__ == "__main__":
     #  TRY ARGS
-    make_image_from_fonts()
-   
+    make_image_from_fonts("image_vermei.JPG", _to="arquivo_saida.jpg")
+    input()
     _file_ = None
     try:
         __ = argv[1]
