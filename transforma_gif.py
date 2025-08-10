12cfe398b7d1dd9b9241e55e1d0cf6de6b55bc58
ddr669 <dudu.herique.x@gmail.com>
2025-08-10 05:49:06 -0300

    feature video edit with text and audio

--- transforma_gif.py
+++ transforma_gif.py
@@ -5,7 +5,9 @@
 ###################
 ###### Imports #######
 from PIL import Image
-import cv2 
+import cv2
+
+
 import numpy as np
 from sys import argv
 from pygame import Surface, surfarray, SRCALPHA, font, draw, Rect, image
@@ -14,7 +16,10 @@ from pygame.sprite import Sprite, Group
 from cmdline_verify import cmdline_verify
 from cmdline_verify import __help__, return_file_
 from random import randint
+from moviepy import VideoClip,AudioFileClip, CompositeAudioClip,CompositeVideoClip, ImageSequenceClip
 from os import listdir as os_path
+from time import time as time_now
+
 ###### Sprites #######
 class Sprites_(Sprite):
     def __init__(self, color=(0,0,0,0), height: int = None, width: int = None,
@@ -23,56 +28,137 @@ class Sprites_(Sprite):
         if type(_file) == Image.Image:
             self.image = image.frombytes(_file.tobytes(), _file.size, 'RGB')
         else:
-            
             self.image = image.load(_file) if _file else Surface([width, height])
-        
         if width:
             draw.rect(self.image,color,Rect(0,0,width,height))
-
         self.rect = self.image.get_rect()
         if self.rect[-1] >= 720 or self.rect[-2] >= 1024:
             self.image = scale(self.image, (720, 500))
             self.rect = self.image.get_rect()
-        #self.image = scale(self.image, (320, 180))
 
+DEBUG_INFO = 1
+
+
+def make_video_from_video(
+                        file, out: str = "video_as_gif.gif",
+                        frame_counter: int = 90, text: str = "10",
+                        font_color: tuple = (0,0,0),
+                        lower_color: list = np.array([140,140,140]),
+                        upper_color: list = np.array([220,220,220]),
+                        start_clip: int = 0,
+                        end_clip: int = 90,
+                        remove_bg: bool = False,
+                        new_bg_color: tuple = (),
+                        ):
+    
+    global DEBUG_INFO
+
+    if DEBUG_INFO:
+        print(f"[VideoReader with open-cv2]: Ini")
+    init_time = time_now()
+    cap = cv2.VideoCapture(file)
+    
+    
+    counter = 0
+    sub_counter = 0 
+    FRAMES = []
+    CLIPS = []
+    while cap.isOpened():
+        ret, cv2_frame = cap.read()
+        
+        
+        if ret:
+            converted = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB)
+            pil_image = Image.fromarray(converted)
+            FRAMES.append(make_image_from_fonts(pil_image,
+                                                text=text,
+                                                font_color=font_color,
+                                                lower_target=lower_color,
+                                                upper_target=upper_color,
+                                                remove_bg=remove_bg,
+                                                new_bg_color=new_bg_color))
+         
+
+        elif ret:
+            pass
+        else:
+            break
+        counter += 1
+        sub_counter += 1
+        if counter >= frame_counter:
+            break
+    cap.release() 
+   
+    #Image.Image.width
+    #Image.Image.getdata()
+    if DEBUG_INFO:
+        print("[!done load array in heap]")
+
+    frames = [np.array(_) for _ in FRAMES]
+    scene = ImageSequenceClip(frames, fps=60)
+    audioclip = AudioFileClip(file)
+    audioclip.duration = scene.duration
+    scene.audio = audioclip
+    
+    scene.write_videofile(out)
+    #final_video = CompositeVideoClip([clip, audioclip])
+    #final_video.write_videofile(filename=out,audio=True)
+
+
+    #frame0 = FRAMES[0]
+    #frame0.save(f"out/{out}", save_all=True,append_images=FRAMES,duration=frame_counter, loop=0)
+
+    now_time = time_now()
+    tempo_de_exec = now_time - init_time
+    if DEBUG_INFO:
+        print(f"[VideoReader with open-cv2]: time elapsed -> {tempo_de_exec}")
+    
 def make_gif_from_video(
                         file, out: str = "video_as_gif.gif",
                         frame_counter: int = 90, text: str = "10",
                         font_color: tuple = (0,0,0),
                         lower_color: list = np.array([140,140,140]),
-                        upper_color: list = np.array([220,220,220])
-                        ):   
-    
-   
+                        upper_color: list = np.array([220,220,220]),
+                        remove_bg: bool = False,
+                        new_bg_color: tuple = (),
+                        ):
+    global DEBUG_INFO
+    if DEBUG_INFO:
+        print(f"[VideoReader with open-cv2]: Ini")
+    init_time = time_now()
     cap = cv2.VideoCapture(file)
-    cap.set(cv2.CAP_PROP_FPS, 60.0)
-    cap.set(cv2.CAP_PROP_VIDEO_STREAM, 24)
-    cap.set(cv2.CAP_PROP_HW_ACCELERATION, 1)
     counter = 0
+    sub_counter = 0 
     FRAMES = []
-
     while cap.isOpened():
         ret, cv2_frame = cap.read()
-        if ret:
+        if ret and sub_counter % 3 == 0:
             converted = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2RGB)
             pil_image = Image.fromarray(converted)
-            FRAMES.append(make_image_from_fonts(pil_image, text=text, font_color=font_color, lower_target=lower_color, upper_target=upper_color))
-            cv2.waitKey(0)
-            
+            FRAMES.append(make_image_from_fonts(pil_image,
+                                                text=text,
+                                                font_color=font_color,
+                                                lower_target=lower_color,
+                                                upper_target=upper_color,
+                                                remove_bg=remove_bg,
+                                                new_bg_color=new_bg_color))
+        elif ret:
+            pass
         else:
             break
         counter += 1
+        sub_counter += 1
         if counter >= frame_counter:
             break
     cap.release()
-    
     frame0 = FRAMES[0]
+    frame0.save(f"out/{out}", save_all=True,append_images=FRAMES,duration=frame_counter, loop=0)
+    now_time = time_now()
+    tempo_de_exec = now_time - init_time
+    if DEBUG_INFO:
+        print(f"[VideoReader with open-cv2]: time elapsed -> {tempo_de_exec}")
     
-    frame0.save(f"out/{out}", save_all=True, append_images=FRAMES,duration=90, loop=0)
 
-# xxxxxxxxxxxxxxxxx
-# make a texture to image with pygame.Surface
-# from the given args (_font, _size, _from, _to, size:tuple=(20,20) repeat: bool=False, text: str= "xxx")
 def make_image_from_fonts(file,
                         font_family: str = "lucidaconsole", 
                         font_size: int = 12,
@@ -82,41 +168,31 @@ def make_image_from_fonts(file,
                         text: str = None,
                         remove_bg: bool = False,
                         new_bg_color: tuple = (0,0,0)) :
-    """
-        file - file to edit
-        _font - font-family 
-        _size - font-size
-        _to - !
-        size - !
-        _text - if a _text will type the text a lot of times
-        _color_ - !
-        _remore_ - to overwrite the color range to _color_
-
-    """
-    
+    '''*make_image_from_fonts -
+            Takes a file object or PIL.Image object 
+            with argument and some parameters to return a PIL.Image
+            overwrite the pixels in range color between lower and
+            upper target.
+            - font_family: str, default="lucidaconsole".
+            - font_size: int, default=12. # thats is perfect
+            - font_color: tuple, default=(255,255,255).
+            - lower_target: list, default=numpy.array([0,0,0]) # black
+            - upper_target: list, default=numpy.array([45,45,45]) # gray
+            - text: str, default=["1","0"]
+            - remove_bg: bool, default=False.
+            - new_bg_color: tuple = (0,0,0).'''
     sprite_group = Group()
     _img = Sprites_(_file=file)
     sprite_group.add(_img)
     size = _img.image.get_rect()[2:]
-    
     arraysurf = Surface((size[0],size[1]), SRCALPHA)
     font.init()
     font_pygame = font.SysFont(font_family, font_size)
-
     texto = font_pygame.render(text if text else "", True, font_color)
     arraysurf.fill((0,0,0,0))
-        
     sprite_group.draw(arraysurf)
     _tmp = surfarray.array3d(arraysurf)
-    
-    lower_target = lower_target 
-    upper_target = upper_target 
-
     chars_ = ["0", "1"] if not text else [letter for letter in text] # ░
-    # todo: 
-    #   algorithm to separate 1/4 of frame and multithread to process it
-    #
-
     for a in range(1, size[1], 12):
         for x in range(1, size[0],8):
                 r,g,b = _tmp[x][a]
@@ -124,17 +200,14 @@ def make_image_from_fonts(file,
                     if remove_bg:
                         draw.rect(arraysurf,new_bg_color, Rect(x, a, 12, 12)) 
                     arraysurf.blit(texto, (x,a))
-
-
                 _text = f"{chars_[randint(0, len(chars_)-1)]}"
                 texto = font_pygame.render(_text, True, font_color)
-
     _tmp = surfarray.array3d(arraysurf)
     imagem_com_texto = Image.fromarray(_tmp)
-   
     view = imagem_com_texto.transpose(Image.Transpose.TRANSPOSE)
-    view = view.quantize(method=2, colors=82, dither=Image.Dither.RASTERIZE).convert('RGB')
-
+    view = view.quantize(method=2,colors=140, dither=Image.Dither.RASTERIZE).convert('RGB')
+    del(sprite_group)
+    del(_img, arraysurf, font_pygame, texto, _tmp, imagem_com_texto)
     return view
     
 
@@ -147,54 +220,42 @@ def save_as_gif(file, text: str = 'x;',
                 font_color: tuple = (255,255,255),
                 font_family: str = 'lucidaconsola'):
     ''' save files from path to save as gif in out '''
-    if target:
-        try:
-            lower_target = np.array(target[0])
-            upper_target = np.array(target[1])
-        except IndexError as err:
-            print(f'[!] IndexError: target must be a list of two tuple/list.\n{err}')
-            lower_target = np.array([0,0,0])
-            upper_target = np.array([45,45,45])
-    else:
+    try:
+        lower_target = np.array(target[0])
+        upper_target = np.array(target[1])
+    except IndexError as err:
+        print(f'[!] IndexError: target must be a list of two tuple/list.\n{err}')
         lower_target = np.array([0,0,0])
         upper_target = np.array([45,45,45])
-    if remove_bg:
-        frames = [make_image_from_fonts(file=file,
-                                        text=text,
-                                        lower_target=lower_target,
-                                        upper_target=upper_target,
-                                        remove_bg=True,
-                                        new_bg_color=new_bg_color,
-                                        font_color=font_color,
-                                        font_family=font_family) for _ in range(0, img_count)]
-    else:
-        frames = [make_image_from_fonts(file=file,
-                                        text=text,
-                                        lower_target=lower_target,
-                                        upper_target=upper_target,
-                                        font_color=font_color,
-                                        font_family=font_family)]
+    
+    frames = [make_image_from_fonts(file=file,
+                                    text=text,
+                                    lower_target=lower_target,
+                                    upper_target=upper_target,
+                                    remove_bg=remove_bg,
+                                    new_bg_color=new_bg_color,
+                                    font_color=font_color,
+                                    font_family=font_family) for _ in range(0, img_count)]
+    
     frame0 = frames[0]
     frame0.save(f"out/{out}", save_all=True, append_images=frames, duration=90, loop=0)
 
 def main(file_dict: dict):
-    
     if file_dict == "!":
         __help__()
         exit()
   
     if file_dict["GUI"]:
         pass
- 
-    # END
-if __name__ == "__main__":
-    make_gif_from_video('jamelao.mp4', out="jamelao_new_altobitrate.gif",
-                        lower_color=np.array([0,0,0]),
-                        upper_color=np.array([45,45,45]),
-                        font_color=(250,0,20))
-    #save_as_gif('files/out_0.jpg')
-    print("[*] done.")
 
+if __name__ == "__main__":
+    make_video_from_video('jamelao.mp4', out="out/jamelao_finaly_mamae.mp4",
+                        lower_color=np.array([0,0,0]),
+                        upper_color=np.array([120,105,145]),
+                        remove_bg=False,frame_counter=600,
+                        font_color=(250,0,0))
+
+    print("[*] done.")
     input()
     _file_ = None
     try:
@@ -204,5 +265,5 @@ if __name__ == "__main__":
     except IndexError as Err:
         _file_ = return_file_()
    
-    app = main(_file_) #  if _file_["path"] else main("!")
+    app = main(_file_)
     
