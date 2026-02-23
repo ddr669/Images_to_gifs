77ae25e0f39d7224b58143feab26ea18d544a7d7
Eduardo Henrique <dudu.herique.x@gmail.com>
2026-02-23 22:17:56 +0000

    removing some comments

--- modules/array_module.py
+++ modules/array_module.py
@@ -47,19 +47,7 @@ def return_image_from_array(array: cv2.Mat | np.ndarray) -> Image.Image:
 def create_mask(file_,
                 lower_target: np.array = np.array([0,0,0]),
                 upper_target: np.array = np.array([11,11,11])
-                ):
-        ''' 
-        Using cv2 to select a mask inRange  
-        from a image file or frame.  
-
-        Parameters
-        ----------
-            #file (PIL.Image.Image | str | cv2.Mat): File Image.
-            lower_target (np.array) = np.array([0,0,0]): lower color range.
-            upper_target (np.array) = np.array([11,11,11]): upper color range.
-        Returns:
-            cv2.Mat: Image array like
-        '''
+                ): 
         
         lower_target, upper_target = sanitize_ranges(lower_target, upper_target)
         mask = cv2.inRange(file_, lower_target, upper_target)
