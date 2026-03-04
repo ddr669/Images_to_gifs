8e262c767e5e2386aca0349db6e2104d2cc75898
ddr669 <dudu.herique.x@gmail.com>
2026-03-04 02:49:40 -0300

    removing some unexpected function on dunder init

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,13 @@
 #-*-By:__DDr669__-*-
 #-*-Date:__/__/__-*-
 #from .config_variables import * 
-from .modules import * 
-from .cmdline_controll import *
+#from .modules import * 
+#from .cmdline_controll import *
+from .image_class_module import *
 from PIL import Image, PngImagePlugin, JpegImagePlugin, ImageDraw, ImageFont
 import cv2
 import numpy as np
 from sys import argv
-from random import randint
 #from os import listdir as os_path
 #from os.path import exists as path_exist
 #from os import getcwd as GETPWD
