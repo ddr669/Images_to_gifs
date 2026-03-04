79a7ca892e0edfc0b7de17925fc6c7f20e7cce3d
ddr669 <dudu.herique.x@gmail.com>
2026-03-04 01:17:45 -0300

    removing deprecate dependency

--- src/__init__.py
+++ src/__init__.py
@@ -2,25 +2,15 @@
 #-*-encode: utf-8-*-
 #-*-By:__DDr669__-*-
 #-*-Date:__/__/__-*-
-from .config_variables import * 
-
+#from .config_variables import * 
+from .modules import * 
 from .cmdline_controll import *
-
 from PIL import Image, PngImagePlugin, JpegImagePlugin, ImageDraw, ImageFont
 import cv2
 import numpy as np
 from sys import argv
-
-
-from pygame import Surface, surfarray, SRCALPHA, font, draw, Rect, image
-from pygame.transform import scale
-from pygame.sprite import Sprite, Group
-
 from random import randint
-from moviepy import AudioFileClip, ImageSequenceClip
-
-
-from os import listdir as os_path
-from os.path import exists as path_exist
-from os import getcwd as GETPWD
+#from os import listdir as os_path
+#from os.path import exists as path_exist
+#from os import getcwd as GETPWD
 from time import time as time_now
